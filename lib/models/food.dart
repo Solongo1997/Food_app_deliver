b08ade04758e4843a18f20debde4648562fbdde6
nezukkk <56289331+Solongo1997@users.noreply.github.com>
2022-04-11 00:56:53 -0700

    add Popular food

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -47,6 +47,27 @@ class Food {
       'simply put, This is Rameeen'
       ),
     ];
-    
-    }}
+    }  static List<Food> generatePopularFoods(){
+      return[
+         Food( 'assets/images/dish4.png', 'MOst Popular', 'Soba soup', '50min', 5, '330 kcal', 12, 1,
+      [
+        {'Noodle': 'assets/images/ingre1.png',},
+        { 'Shrimp': 'assets/images/ingre2.png',},
+        {'Egg': 'assets/images/ingre3.png',},
+        {'Scallion': 'assets/images/ingre4.png',},
+      ],
+      'simply put, This is Rameeen'
+      ),
+        Food( 'assets/images/dish1.png', 'MOst Popular', 'Soba soup', '50min', 5, '330 kcal', 12, 1,
+      [
+        {'Noodle': 'assets/images/ingre1.png',},
+        { 'Shrimp': 'assets/images/ingre2.png',},
+        {'Egg': 'assets/images/ingre3.png',},
+        {'Scallion': 'assets/images/ingre4.png',},
+      ],
+      'simply put, This is Rameeen'
+      ),
+      ];
+    }
+    }
     
